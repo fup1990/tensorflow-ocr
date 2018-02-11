f860adeb30afeca271fd16f12692684c88aecbf3
fup1990 <fup1990@163.com>
2018-02-11 18:00:10 +0800

    批量获取测试数据

---
+++ cnn_train.py
@@ -0,0 +1,33 @@
+import numpy as np
+import tensorflow as tf
+import gen_captcha as gc
+import word_vec as wv
+
+# 图像大小
+IMAGE_HEIGHT = 60
+IMAGE_WIDTH = 160
+# 字符数量
+WORD_NUM = 4
+
+def next_batch(batch_size=64):
+    # 图片数据
+    batch_x = np.zeros((batch_size, IMAGE_WIDTH * IMAGE_HEIGHT))
+    # 文字数据
+    batch_y = np.zeros((batch_size, WORD_NUM * wv.CHAR_NUM))
+
+    for i in range(batch_size):
+        text, image = gc.captcha_text_image(WORD_NUM)
+        # 一维化
+        image = image.flatten() / 256
+        batch_x[i, :] = image
+        vec = wv.word2vec(text)
+        v = vec.flatten()
+        batch_y[i, :] = vec.flatten()
+
+    return batch_x, batch_y
+
+
+
+batch_x, batch_y = next_batch()
+print(batch_x)
+print(batch_y)

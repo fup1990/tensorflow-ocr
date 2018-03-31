5021f156a22ff33a86825414bc2f7b3988bb21e9
fup1990 <fup1990@163.com>
2018-03-31 21:43:59 +0800

    实现crnn模型

--- crnn_model.py
+++ crnn_model.py
@@ -1,27 +1,102 @@
 import tensorflow as tf
-slim = tf.contrib.slim
-layers = tf.layers
+import numpy as np
 
-is_training = True
+def conv2d(input_data, out_channel, ksize=3, strides=1, padding=1, w_init=None, b_init=None, name=None):
 
-x_data = tf.placeholder(dtype=tf.float32, shape=[None, None, 32])
+    in_shape = input_data.get_shape().as_list()
+
+    if padding == 1:
+        padding = 'VALID'
+    else:
+        padding = 'SAME'
+
+    if isinstance(ksize, list):
+        # in_shape[3]获取图片的深度
+        filter = ksize + [in_shape[3], out_channel]
+    else:
+        filter = [ksize, ksize, in_shape[3], out_channel]
+
+    if isinstance(strides, list):
+        strides = [1, strides[0], strides[1], 1]
+    else:
+        strides = [1, strides, strides, 1]
+
+    if w_init is None:
+        # he initial
+        w_init = tf.contrib.layers.variance_scaling_initializer()
+    if b_init is None:
+        b_init = tf.constant_initializer()
+
+    weights = tf.get_variable("weights", filter, initializer=w_init)
+    bias = tf.get_variable('bias', [out_channel], initializer=b_init)
+
+    conv = tf.nn.conv2d(input_data, weights, strides, padding, name=name)
+
+    return relu(tf.nn.bias_add(conv, bias=bias))
+
+def max_pooling(value, ksize=2, strides=2, padding=0, data_format="NHWC", name=None):
+
+    if padding == 1:
+        padding = 'VALID'
+    else:
+        padding = 'SAME'
+
+    if isinstance(ksize, list):
+        ksize = [1, ksize[0], ksize[1], 1]
+    else:
+        ksize = [1, ksize, ksize, 1]
+
+    if strides is None:
+        strides = ksize
+
+    if isinstance(strides, list):
+        strides = [1, strides[0], strides[1], 1]
+    else:
+        strides = [1, strides, strides, 1]
+
+    return tf.nn.max_pool(value, ksize, strides, padding, data_format=data_format, name=name)
+
+def relu(features, name=None):
+    return tf.nn.relu(features, name=name)
+
+def batch_norm(input_data):
+    return tf.contrib.layers.batch_norm(input_data, is_training=True)
 
 # CNN
-with slim.arg_scope([slim.conv2d], stride=1, padding='VALID'):
-    conv1 = slim.conv2d(x_data, 64, [3, 3], scope='conv1')
-    pool2 = slim.max_pool2d(conv1, [2, 2], stride=2, scope='pool2')
-    conv3 = slim.conv2d(pool2, 128, [3, 3], scope='conv3')
-    pool4 = slim.max_pool2d(conv3, [2, 2], stride=2, scope='pool4')
-    conv5 = slim.conv2d(pool4, 256, [3, 3], scope='conv5')
-    conv6 = slim.conv2d(conv5, 256, [3, 3], scope='conv6')
-    pool7 = slim.max_pool2d(conv6, [1, 2], stride=2, scope='pool7')
-    conv8 = slim.conv2d(pool7, 512, [3, 3], normalizer_fn=slim.batch_norm, normalizer_params={'is_training': is_training}, scope='conv8')
-    conv10 = slim.conv2d(conv8, 512, [3, 3], normalizer_fn=slim.batch_norm, normalizer_params={'is_training': is_training}, scope='conv10')
-    pool12 = slim.max_pool2d(conv10, [1, 2], stride=2, scope='pool12')
-    conv13 = slim.conv2d(pool12, 512, [2, 2], padding='SAME', scope='conv13')
+def conv(input_data):
+    """
+    :param input_data:batch*32*100*3
+    :return: output_data:batch*1*25*512
+    """
+    conv1 = conv2d(input_data, out_channel=64)
+    pool2 = max_pooling(conv1)                                          # batch*16*50*64
+    conv3 = conv2d(pool2, out_channel=128)
+    pool4 = max_pooling(conv3)                                          # batch*8*25*128
+    conv5 = conv2d(pool4, out_channel=256)
+    conv6 = conv2d(conv5, out_channel=256)
+    pool7 = max_pooling(conv6, ksize=[2, 1], strides=[2, 1])            # batch*4*25*256
+    conv8 = conv2d(pool7, out_channel=512)
+    bn9 = batch_norm(conv8)
+    conv10 = conv2d(bn9, out_channel=512)
+    bn11 = batch_norm(conv10)
+    pool12 = max_pooling(bn11, ksize=[2, 1], strides=[2, 1])            # batch*2*25*512
+    conv13 = conv2d(pool12, out_channel=512, ksize=2, strides=[2, 1], padding=0)
+    return conv13
 
 # Map-to-Sequence
+def map_to_sequence(input_data):
+    """
+    :param input_data:batch*1*25*512
+    :return:output_data:batch*25*512
+    """
+    shape = input_data.get_shape().as_list()
+    assert shape[1] == 1
+    # 从tensor中删除所有大小是1的维度
+    return tf.squeeze(input_data)
 
-# RNN
+# BiRNN
+def birnn():
+
 
 # Transcription
+

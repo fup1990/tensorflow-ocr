4bac75a8b528fbadaad362bc10e2ad787813cb19
fup1990 <fup1990@163.com>
2018-04-02 17:56:00 +0800

    CRNN优化

--- crnn_model.py
+++ crnn_model.py
@@ -2,38 +2,37 @@ import tensorflow as tf
 from tensorflow.contrib import rnn
 import numpy as np
 
-def conv2d(input_data, out_channel, ksize=3, strides=1, padding=0, w_init=None, b_init=None, name=None):
+def conv2d(input_data, out_channel, name, ksize=3, strides=1, padding=0, w_init=None, b_init=None):
 
-    in_shape = input_data.get_shape().as_list()
+    with tf.variable_scope(name):
+        in_shape = input_data.get_shape().as_list()
 
-    if padding == 1:
-        padding = 'VALID'
-    else:
-        padding = 'SAME'
+        if padding == 1:
+            padding = 'VALID'
+        else:
+            padding = 'SAME'
 
-    if isinstance(ksize, list):
-        # in_shape[3]获取图片的深度
-        filter = ksize + [in_shape[3], out_channel]
-    else:
-        filter = [ksize, ksize, in_shape[3], out_channel]
+        if isinstance(ksize, list):
+            # in_shape[3]获取图片的深度
+            filter = ksize + [in_shape[3], out_channel]
+        else:
+            filter = [ksize, ksize, in_shape[3], out_channel]
 
-    if isinstance(strides, list):
-        strides = [1, strides[0], strides[1], 1]
-    else:
-        strides = [1, strides, strides, 1]
+        if isinstance(strides, list):
+            strides = [1, strides[0], strides[1], 1]
+        else:
+            strides = [1, strides, strides, 1]
 
-    if w_init is None:
-        # he initial
-        w_init = tf.contrib.layers.variance_scaling_initializer()
-    if b_init is None:
-        b_init = tf.constant_initializer()
+        if w_init is None:
+            # he initial
+            w_init = tf.contrib.layers.variance_scaling_initializer()
+        if b_init is None:
+            b_init = tf.constant_initializer()
 
-    weights = tf.Variable(tf.random_normal(filter, mean=0.0, stddev=1.0, dtype=tf.float32))
-    bias = tf.Variable(tf.random_normal([out_channel], mean=0.0, stddev=1.0, dtype=tf.float32))
-    # weights = tf.get_variable("weights", filter, initializer=w_init)
-    # bias = tf.get_variable('bias', [out_channel], initializer=b_init)
+        weights = tf.get_variable("weights", filter, initializer=w_init)
+        bias = tf.get_variable('bias', [out_channel], initializer=b_init)
 
-    conv = tf.nn.conv2d(input_data, weights, strides, padding, name=name)
+        conv = tf.nn.conv2d(input_data, weights, strides, padding, name=name)
 
     return relu(tf.nn.bias_add(conv, bias=bias))
 
@@ -71,19 +70,19 @@ def conv(input_data):
     :param input_data:batch*32*100*3
     :return: output_data:batch*1*25*512
     """
-    conv1 = conv2d(input_data, out_channel=64)
+    conv1 = conv2d(input_data, out_channel=64, name='conv1')
     pool2 = max_pooling(conv1)                                          # batch*16*50*64
-    conv3 = conv2d(pool2, out_channel=128)
+    conv3 = conv2d(pool2, out_channel=128, name='conv3')
     pool4 = max_pooling(conv3)                                          # batch*8*25*128
-    conv5 = conv2d(pool4, out_channel=256)
-    conv6 = conv2d(conv5, out_channel=256)
+    conv5 = conv2d(pool4, out_channel=256, name='conv5')
+    conv6 = conv2d(conv5, out_channel=256, name='conv6')
     pool7 = max_pooling(conv6, ksize=[2, 1], strides=[2, 1])            # batch*4*25*256
-    conv8 = conv2d(pool7, out_channel=512)
+    conv8 = conv2d(pool7, out_channel=512, name='conv8')
     bn9 = batch_norm(conv8)
-    conv10 = conv2d(bn9, out_channel=512)
+    conv10 = conv2d(bn9, out_channel=512, name='conv10')
     bn11 = batch_norm(conv10)
     pool12 = max_pooling(bn11, ksize=[2, 1], strides=[2, 1])            # batch*2*25*512
-    conv13 = conv2d(pool12, out_channel=512, ksize=2, strides=[2, 1], padding=0)    # batch*1*25*512
+    conv13 = conv2d(pool12, out_channel=512, ksize=2, strides=[2, 1], padding=0, name='conv13')    # batch*1*25*512
     return conv13
 
 # Map-to-Sequence
@@ -127,6 +126,7 @@ def test():
         init = tf.global_variables_initializer()
         sess.run(init)
         rnn, l = sess.run(r, feed_dict={input_data: data})
-
+        print(rnn)
+        print(l)
 
 test()
